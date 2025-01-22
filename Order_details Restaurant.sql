7ab94194d99d361cce065c86169087ae47f2b754
Enyonamm <149742560+Enyonamm@users.noreply.github.com>
2025-01-22 13:33:15 +0000

    Add files via upload

---
+++ Order_details Restaurant.sql
@@ -0,0 +1,9 @@
+-- 1. View the order details table.
+SELECT * FROM order_details;
+-- 2. What is the date range of the table?
+
+-- 3. How many orders were made within this date range?
+
+-- 4. Which orders had the most number of items?
+
+-- 5. How many orders had more than 12 items?
